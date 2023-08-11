58390cb806faa4c490cb83b39d714d1b50ba6966
Vassily <vassily.v.v@gmail.com>
2023-08-11 13:03:21 +0600

    init commit

---
+++ src/to_js.dart
@@ -0,0 +1,28 @@
+import 'dart:js' as js;
+import 'dart:js_interop';
+import 'dart:html';
+import 'dart:convert';
+import 'dart:js_util';
+
+import 'api_client.dart';
+
+@JS('Promise')
+class Promise<T> {
+  external Promise(void executor(void resolve(T result), Function reject));
+  external Promise then(void onFulfilled(T result), [Function onRejected]);
+}
+
+Promise performRequest(String functionName, String jsonArgs) {
+  return Promise(js.allowInterop((resolve, reject) {
+    apiMethods[functionName]!(jsonDecode(jsonArgs)).then((value) {
+      final jsonString = jsonEncode(value);
+      resolve(jsonString);
+    }).catchError((error) {
+      reject(error);
+    });
+  }));
+}
+
+void main() {
+  setProperty(window, 'perform', js.allowInterop(performRequest));
+}
